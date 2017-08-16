6d64ebf4bab381ffd5b413d2f6cdc1afe06aa867
ClintonBrown <clintonrbrown@gmail.com>
2017-08-15 21:43:59 -0600

    Added variables to player class to track pixels moved and generate a random number for battle system encounters

--- scripts/player.lua
+++ scripts/player.lua
@@ -14,6 +14,8 @@ function Player:Create(img, nm)
 				   health   = 20,
 				   attack   = 5,
 				   defense  = 5,
+				   moved    = 0,
+				   battle_chance = love.math.random(16 ,80),
 				   can_act = true }
 
 	setmetatable(this, self)
@@ -48,15 +50,19 @@ function Player:Movement(dt, map_w, map_h, c_map)
 		-- move if wsad or arrow keys are pressed but do not leave screen
 		if (key.isDown("w") or key.isDown("up")) and not is_collision.up then
 			self.ypos = math.floor(self.ypos) - (self.speed * dt)
+			self.moved = self.moved + 1
 			
 		elseif (key.isDown("s") or key.isDown("down")) and not is_collision.down then
 			self.ypos = math.ceil(self.ypos) + (self.speed * dt)
+			self.moved = self.moved + 1
 			
 		elseif (key.isDown("a") or key.isDown("left")) and not is_collision.left then
 			self.xpos = math.floor(self.xpos) - (self.speed * dt)
+			self.moved = self.moved + 1
 			
 		elseif (key.isDown("d") or key.isDown("right")) and not is_collision.right then
 			self.xpos = math.ceil(self.xpos) + (self.speed * dt)
+			self.moved = self.moved + 1
 		end
 		
 		-- move collider to new player location
